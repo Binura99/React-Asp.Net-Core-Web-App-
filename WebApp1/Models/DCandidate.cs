c6713d34061c1eac3f754f061c4f800589cc8373
Binura99 <binurayasith12@gmail.com>
2024-06-19 22:31:05 +0530

    Did some changes

---
+++ WebApp1/Models/DCandidate.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp1.Models
+{
+    public class DCandidate
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string fullName { get; set; }
+
+        public string mobile { get; set; }
+
+        public string email { get; set; }
+
+        public string age { get; set; }
+
+        public string bloodGroup { get; set; }
+
+        public string address { get; set; }
+    }
+}
